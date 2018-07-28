a8d974af4b0043daa0896ba0a5cc308683680fc0
ptflp <filippov.pa@s-vfu.ru>
2018-07-29 00:22:54 +0900

    routes test

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,8 @@
 <?php
+/*
+* Example searching rule regexp '([a-z])' => 'news/view' replace path name
+*/
 return Array (
-	'news/([0-9]+)' => 'news/view',
+	'news/([a-z]+)/([0-9]+)' => 'news/view',
 	'news' => 'news/index',
 );
