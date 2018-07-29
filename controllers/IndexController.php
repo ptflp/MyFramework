40fb7dcbbc3fcaa3dc8be23f277b1890eb570b70
ptflp <filippov.pa@s-vfu.ru>
2018-07-29 15:51:01 +0900

    view engine created

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,9 +5,9 @@ require_once ROOT.'/../res/Controller.php';
  */
 class IndexController extends Controller
 {
-	public function actionIndex($id)
+	public function actionIndex($id=false)
 	{
-		echo $id;
-		echo 'Test';
+		$this->view->message = 'Hello World ';
+		$this->view->render('index/index.php');
 	}
 }
--- res/View.php
+++ res/View.php
@@ -14,6 +14,6 @@ class View
 	}
 	public function render($viewScript)
 	{
-		require($viewScript);
+		include_once ROOT.'/../views/'.$viewScript;
 	}
 }
