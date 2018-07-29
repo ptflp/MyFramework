2d7cbe495caba9742a33a7508446c0be1de9b58d
ptflp <filippov.pa@s-vfu.ru>
2018-07-29 15:16:02 +0900

    add mustache engine templater

--- res/View.php
+++ res/View.php
@@ -4,10 +4,12 @@
  */
 class View
 {
-
+	public $mustache;
 	function __construct()
 	{
-		
+		if (class_exists('Mustache_Engine')) {
+		    $this->mustache = new Mustache_Engine;
+		}
 	}
 	public function render($viewScript)
 	{
