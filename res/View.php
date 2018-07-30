1812be65787a75977b574adecc58c8c490f72278
ptflp <filippov.pa@s-vfu.ru>
2018-07-30 14:27:50 +0900

    layouts mechanism added

--- res/View.php
+++ res/View.php
@@ -11,6 +11,7 @@ use Mustache_Loader_FilesystemLoader;
 		private $mustache;
 		public $message;
 		public $param=[];
+		public $layout;
 		function __construct()
 		{
 			if (class_exists('Mustache_Engine')) {
@@ -19,6 +20,7 @@ use Mustache_Loader_FilesystemLoader;
 										'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/../views',$options),
 									));
 			}
+			$this->layout='main';
 		}
 		public function render($viewScript,$param=false)
 		{
@@ -28,7 +30,9 @@ use Mustache_Loader_FilesystemLoader;
 		public function muRender($template,$data)
 		{
 	 		$view=$this->mustache->loadTemplate($template);
-	 		return $view->render($data);
+	 		$content['content']=$view->render($data);
+	 		$layout=$this->mustache->loadTemplate('layouts/main');
+	 		return $layout->render($content);
 		}
 	}
 }
