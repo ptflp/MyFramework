509dde34b06766a366f5b7fd359b08e0a0d3d2a4
ptflp <filippov.pa@s-vfu.ru>
2018-08-01 17:25:20 +0900

    add public route

--- res/Controller.php
+++ res/Controller.php
@@ -8,9 +8,16 @@ namespace res;
 	abstract class Controller
 	{
 		public $view;
+		public $route;
+
 		function __construct()
 		{
 			$this->view = new View();
+	 		if (!empty($_GET['__route'])) {
+	 			$this->route = trim($_GET['__route'],'/');
+	 		} else {
+				$this->route = '/';
+	 		}
 		}
 	}
 }
