d397ebe130b2f9d4ffea179403888dac4e118fd5
ptflp <filippov.pa@s-vfu.ru>
2018-08-02 19:05:50 +0900

    minor change

---
+++ App/Entities/UserOrm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entities;
+
+/**
+ * UserOrm
+ */
+class UserOrm
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return UserOrm
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return UserOrm
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
